d332e2b561df29caa618a3783aadb3f0913987ee
David Bojanic <david.bojanic@hotmail.com>
2023-08-24 21:17:59 +0000

    Added SMPLX landmarks.

--- landmark_definitions.py
+++ landmark_definitions.py
@@ -19,7 +19,6 @@ SMPL_LANDMARK_INDICES = {"HEAD_TOP": 412,
                     "RIGHT_FOREARM": 5197,
                     "LEFT_SHOULDER": 3011,
                     "RIGHT_SHOULDER": 6470,
-                    "LEFT_ANKLE": 3334,
                     "LOW_LEFT_HIP": 3134,
                     "LEFT_THIGH": 947,
                     "LEFT_CALF": 1074,
@@ -28,3 +27,33 @@ SMPL_LANDMARK_INDICES = {"HEAD_TOP": 412,
 
 SMPL_LANDMARK_INDICES["HEELS"] = (SMPL_LANDMARK_INDICES["LEFT_HEEL"], 
                                   SMPL_LANDMARK_INDICES["RIGHT_HEEL"])
+
+
+SMPLX_LANDMARK_INDICES = {"HEAD_TOP": 8976,
+                    "HEAD_LEFT_TEMPLE": 1979,
+                    "NECK_ADAM_APPLE": 8940, 
+                    "LEFT_HEEL": 8847,
+                    "RIGHT_HEEL": 8635,
+                    "LEFT_NIPPLE": 3572,
+                    "RIGHT_NIPPLE": 8340,
+
+                    "SHOULDER_TOP": 5616,
+                    "INSEAM_POINT": 5601,
+                    "BELLY_BUTTON": 5939,
+                    "BACK_BELLY_BUTTON": 5941,
+                    "CROTCH": 3797,
+                    "PUBIC_BONE": 5949,
+                    "RIGHT_WRIST": 7449,
+                    "LEFT_WRIST": 4823,
+                    "RIGHT_BICEP": 6788, 
+                    "RIGHT_FOREARM": 7266,
+                    "LEFT_SHOULDER": 4442,
+                    "RIGHT_SHOULDER": 7218, 
+                    "LOW_LEFT_HIP": 4112, 
+                    "LEFT_THIGH": 3577,
+                    "LEFT_CALF": 3732,
+                    "LEFT_ANKLE": 5880
+                    }
+
+SMPLX_LANDMARK_INDICES["HEELS"] = (SMPLX_LANDMARK_INDICES["LEFT_HEEL"], 
+                                  SMPLX_LANDMARK_INDICES["RIGHT_HEEL"])
